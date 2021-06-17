b203c7bba456a0c9169f973beac856148dd47e44
Mohit-Kundu <moh8.kundu@gmail.com>
2021-06-17 14:12:51 +0530

    Added functions to add and get password

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import sqlite3
 
-conn = sqlite3.connect('login.db')
+#change to 'login.db'
+conn = sqlite3.connect(':memory:')
 
 cur = conn.cursor()
 
+''''cur.execute("""CREATE TABLE login (
+                website text,
+                username text,
+                password text
+    )""")'''
+
+#Inserting login details
+def add_password():
+    with conn:
+        website = input('Enter website: ')
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+
+        log = LOGIN('website', 'username', 'password')
+
+        cur.execute("INSERT INTO login VALUES(?, ?, ?)", (website, username, password))
+
+#Retrieving login details
+def get_password():
+    website = input('Enter website: ')
+    username = input('Enter username: ')
+
+    cur.execute("SELECT * FROM login WHERE website = ? AND username = ?", (website, username))
+
+    print(curr.fetchone())
+
+conn.close()
